b0c220d1bc92609b6698e41fd74407ea47c8a332
FacerAin <syw5141@daum.net>
2019-08-28 18:19:51 +0900

    Init App component

---
+++ todolist/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>todoList</h1>
+      <div class="App">
+        {/*Input Form Component */}
+        {/*Todo Card Component */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
