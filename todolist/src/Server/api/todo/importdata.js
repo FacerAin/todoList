d28d6883a0b91eaf9c66a65f2697687ee653f37f
FacerAin <syw5141@daum.net>
2019-09-05 23:37:01 +0900

    Update Server Directory & Todo Add Button Dialog

---
+++ todolist/src/Server/api/todo/importdata.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+console.log('importdata')
+const importdata = (req, res) => {
+    let data = []
+    const getfiledata = () => {
+        return new Promise((resolve, reject) => {
+            fs.readFile('../model/todo.json', (err, data) => {
+                if (err) {
+                    throw err;
+                } else {
+                    data = JSON.parse(data)
+                    resolve(data)
+                    console.log(data)
+                }
+            })
+        })
+    }
+}
+module.exports = importdata
